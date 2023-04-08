98a80975bc27aa9dfcd1d7c2836becdd46ea39df
Torvul <43212694+torvul@users.noreply.github.com>
2023-04-08 19:53:21 -0400

    Add project files.

---
+++ TermProjectAPI/Controllers/ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace TermProjectAPI.Controllers
+{
+    [Route("HomesAPI/")]
+    [Produces("application/json")]
+    public class ValuesController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public List<Home> GetHomes()
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_ALLHOMES";
+
+
+            DataSet ds = objDB.GetDataSet(objCommand);
+
+            List<Home> returnable = new List<Home>();
+
+            
+            foreach(DataRow row in ds.Tables[0].Rows)
+            {
+                Home addable = new Home();
+                addable.Homeaddress = row[0].ToString();
+                
+                returnable.Add(addable);
+
+            }
+
+            return returnable;
+
+        }
+
+        /*
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+        */
+    }
+}
---
+++ TermProjectAPI/Home/Home.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TermProjectAPI
+{
+    public class Home
+    {
+        public String Homeaddress { get; set; }
+        public String SellerUsername { get; set; }
+        public String PropertyType { get; set; }
+        public int HomeSize { get; set; }
+        public int BedRoomNumber { get; set; }
+        public int BathRoomNumber { get; set; }
+        public String Amentities { get; set; }
+        public String Utilities { get; set; }
+        public int Yearbuilt { get; set; }
+        public String Garage { get; set; }
+        public String Description { get; set; }
+        public int AskingPrice { get; set; }
+        public String DateListed { get; set; }
+        public String Photos { get; set; }
+
+
+        public Home()
+        {
+        }
+
+        public Home(String Homeaddress, String SellersUsername, String PropertyType, int HomeSize, int BedRoomNumber, int BathRoomNumber, String Amentities, String Utilities, int Yearbuilt, String Garage, String Description, int AskingPrice, String DateListed, String Photos)
+        {
+            this.Homeaddress = Homeaddress;
+            this.SellerUsername = SellersUsername;
+            this.PropertyType = PropertyType;
+            this.HomeSize = HomeSize;
+            this.BedRoomNumber = BedRoomNumber;
+            this.BathRoomNumber = BathRoomNumber;
+            this.Amentities = Amentities;
+            this.Utilities = Utilities;
+            this.Yearbuilt = Yearbuilt;
+            this.Garage = Garage;
+            this.Description = Description;
+            this.AskingPrice = AskingPrice;
+            this.DateListed = DateListed;
+            this.Photos = Photos;
+
+        }
+
+    }
+}
